1cb60bbf7d34c97ddfa585be27e9fc4230ad919b
GaryJBlake <31245616+GaryJBlake@users.noreply.github.com>
2020-02-18 17:46:37 +0000

    Enhancements to Get-VCFManager
    Consolidation of code around if statements
    Added -domainId input to support ?domain=

--- docs/functions/Foundation/Get-VCFManager.md
+++ docs/functions/Foundation/Get-VCFManager.md
@@ -5,7 +5,7 @@ Get a list of SDDC Managers
 
 ### Syntax
 ```
-Get-VCFManager -id <string>
+Get-VCFManager -id <string> 
 ```
 
 ### Description
@@ -24,10 +24,29 @@ Get-VCFManager -id 60d6b676-47ae-4286-b4fd-287a888fb2d0
 ```
 This example shows how to return the details for a specific SDDC Manager based on the ID  
 
+#### Example 3
+```
+Get-VCFManager -domain 1a6291f2-ed54-4088-910f-ead57b9f9902
+```
+This example shows how to return the details for a specific SDDC Manager based on a domain ID  
+
 ### Parameters
 
 #### -id
 - ID of a specific SDDC Manager
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+```
+
+#### -domainId
+- ID of a specific domain
 
 ```yaml
 Type: String
--- powerVCF.psm1
+++ powerVCF.psm1
@@ -2293,30 +2293,39 @@ Function Get-VCFManager {
   .EXAMPLE
   PS C:\> Get-VCFManager -id 60d6b676-47ae-4286-b4fd-287a888fb2d0
   This example shows how to return the details for a specific SDDC Manager based on the ID
+
+  .EXAMPLE
+  PS C:\> Get-VCFManager -domain 1a6291f2-ed54-4088-910f-ead57b9f9902
+  This example shows how to return the details for a specific SDDC Manager based on a domain ID
 #>
 
   Param (
     [Parameter (Mandatory=$false)]
       [ValidateNotNullOrEmpty()]
-      [string]$id
+      [string]$id,
+    [Parameter (Mandatory=$false)]
+      [ValidateNotNullOrEmpty()]
+      [string]$domainId
   )
 
   $headers = @{"Accept" = "application/json"}
   $headers.Add("Authorization", "Basic $base64AuthInfo")
+  $method = "GET"
 
-  if ($PsBoundParameters.ContainsKey("id")) {
-    $uri = "https://$sddcManager/v1/sddc-managers/$id"
-  }
-  else {
-    $uri = "https://$sddcManager/v1/sddc-managers"
-  }
   try {
     if ($PsBoundParameters.ContainsKey("id")) {
-      $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
+      $uri = "https://$sddcManager/v1/sddc-managers/$id"
+      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
       $response
     }
-    else {
-      $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
+    if (-not $PsBoundParameters.ContainsKey("id") -and (-not $PsBoundParameters.ContainsKey("domainId"))) {
+      $uri = "https://$sddcManager/v1/sddc-managers"
+      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
+      $response.elements
+    }
+    if ($PsBoundParameters.ContainsKey("domainId")) {
+      $uri = "https://$sddcManager/v1/sddc-managers/?domain=$domainId"
+      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
       $response.elements
     }
   }
