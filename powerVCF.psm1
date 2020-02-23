b1d2516e9f50a177c4ff522b0a3c0c670a76bb06
GaryJBlake <31245616+GaryJBlake@users.noreply.github.com>
2020-02-23 19:47:19 +0000

    Code Clean Up Get-VCFPsc

--- powerVCF.psm1
+++ powerVCF.psm1
@@ -2621,31 +2621,26 @@ Function Get-VCFPsc {
 
   # Check the version of SDDC Manager
   CheckVCFVersion
-
-  $headers = @{"Accept" = "application/json"}
-  $headers.Add("Authorization", "Basic $base64AuthInfo")
-  $method = "GET"
-
-  try {
+  Try {
+    createHeader # Calls Function createHeader to set Accept & Authorization
     if (-not $PsBoundParameters.ContainsKey("id") -and (-not $PsBoundParameters.ContainsKey("domainId"))) {
       $uri = "https://$sddcManager/v1/pscs"
-      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
+      $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
       $response.elements
     }
     if ($PsBoundParameters.ContainsKey("id")) {
       $uri = "https://$sddcManager/v1/pscs/$id"
-      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
+      $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
       $response
     }
     if ($PsBoundParameters.ContainsKey("domainId")) {
       $uri = "https://$sddcManager/v1/pscs/?domain=$domainId"
-      $response = Invoke-RestMethod -Method $method -URI $uri -headers $headers
+      $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
       $response.elements
     }
   }
-  catch {
-    # Call the function ResponseException which handles execption messages
-    ResponseException
+  Catch {
+    ResponseException # Call Function ResponseExecption to get error response from the exception
   }
 }
 Export-ModuleMember -Function Get-VCFPsc
