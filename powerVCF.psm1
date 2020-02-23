b47c1a18041ef872a3bda3d56bf1aceec1a98f9e
GaryJBlake <31245616+GaryJBlake@users.noreply.github.com>
2020-02-23 18:57:20 +0000

    Code Clean Up Get-VCFCredential

--- powerVCF.psm1
+++ powerVCF.psm1
@@ -1340,74 +1340,64 @@ Export-ModuleMember -Function Get-VCFCredentialTask
 
 Function Get-VCFCredential {
 <#
-    .SYNOPSIS
-    Connects to the specified SDDC Manager and retrieves a list of credentials.
-    Supported resource types are: PSC, VCENTER, ESXI, NSX_MANAGER, NSX_CONTROLLER, BACKUP
-    Please note: if you are requesting credentials by resource type then the resource name parameter (if passed) will be ignored (they are mutually exclusive)
+  .SYNOPSIS
+  Connects to the specified SDDC Manager and retrieves a list of credentials.
+  Supported resource types are: PSC, VCENTER, ESXI, NSX_MANAGER, NSX_CONTROLLER, BACKUP
+  Please note: if you are requesting credentials by resource type then the resource name parameter (if passed) will be ignored (they are mutually exclusive)
 
-    .DESCRIPTION
-    The Get-VCFCredential cmdlet connects to the specified SDDC Manager and retrieves a list of
-    credentials. A privileged user account is required.
+  .DESCRIPTION
+  The Get-VCFCredential cmdlet connects to the specified SDDC Manager and retrieves a list of
+  credentials. A privileged user account is required.
 
-    .EXAMPLE
-    PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1!
-    This example shows how to get a list of credentials
+  .EXAMPLE
+  PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1!
+  This example shows how to get a list of credentials
 
-    .EXAMPLE
-    PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1! -resourceType PSC
-    This example shows how to get a list of PSC credentials
+  .EXAMPLE
+  PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1! -resourceType PSC
+  This example shows how to get a list of PSC credentials
 
 	.EXAMPLE
-    PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1! -resourceName sfo01m01esx01.sfo.rainpole.local
-    This example shows how to get the credential for a specific resourceName (FQDN)
+  PS C:\> Get-VCFCredential -privilegedUsername sec-admin@rainpole.local -privilegedPassword VMw@re1! -resourceName sfo01m01esx01.sfo.rainpole.local
+  This example shows how to get the credential for a specific resourceName (FQDN)
 #>
 
-	param (
-        [Parameter (Mandatory=$true)]
-            [ValidateNotNullOrEmpty()]
-            [string]$privilegedUsername,
-
-		[Parameter (Mandatory=$true)]
-            [ValidateNotNullOrEmpty()]
-            [string]$privilegedPassword,
-
+	Param (
+    [Parameter (Mandatory=$true)]
+      [ValidateNotNullOrEmpty()]
+      [string]$privilegedUsername,
+    [Parameter (Mandatory=$true)]
+      [ValidateNotNullOrEmpty()]
+      [string]$privilegedPassword,
 		[Parameter (Mandatory=$false)]
-            [ValidateNotNullOrEmpty()]
-            [string]$resourceName,
+      [ValidateNotNullOrEmpty()]
+      [string]$resourceName,
+    [Parameter (Mandatory=$false)]
+      [ValidateSet("PSC", "VCENTER", "ESXI", "NSX_MANAGER", "NSX_CONTROLLER", "BACKUP")]
+      [ValidateNotNullOrEmpty()]
+      [string]$resourceType
+  )
 
-        [Parameter (Mandatory=$false)]
-            [ValidateSet("PSC", "VCENTER", "ESXI", "NSX_MANAGER", "NSX_CONTROLLER", "BACKUP")]
-            [ValidateNotNullOrEmpty()]
-            [string]$resourceType
-    )
-
-    $headers = @{"Accept" = "application/json"}
-    $headers.Add("Authorization", "Basic $base64AuthInfo")
-    $headers.Add("privileged-username", "$privilegedUsername")
-    $headers.Add("privileged-password", "$privilegedPassword")
-
-    if ($PsBoundParameters.ContainsKey("resourceName")) {
-
-        $uri = "https://$sddcManager/v1/credentials?resourceName=$resourceName"
-
-    }
-    else {
-        $uri = "https://$sddcManager/v1/credentials"
-    }
-    # if requesting credential by type then name is ignored (mutually exclusive)
-    if ($PsBoundParameters.ContainsKey("resourceType") ) {
-
-        $uri = "https://$sddcManager/v1/credentials?resourceType=$resourceType"
-    }
-
-    Try {
-	    $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
-	    $response
-    }
-    Catch {
-        #Get response from the exception
-        ResponseException
-    }
+  createHeader # Calls Function createHeader to set Accept & Authorization
+  $headers.Add("privileged-username", "$privilegedUsername")
+  $headers.Add("privileged-password", "$privilegedPassword")
+  if ($PsBoundParameters.ContainsKey("resourceName")) {
+    $uri = "https://$sddcManager/v1/credentials?resourceName=$resourceName"
+  }
+  else {
+    $uri = "https://$sddcManager/v1/credentials"
+  }
+  # if requesting credential by type then name is ignored (mutually exclusive)
+  if ($PsBoundParameters.ContainsKey("resourceType") ) {
+    $uri = "https://$sddcManager/v1/credentials?resourceType=$resourceType"
+  }
+  Try {
+    $response = Invoke-RestMethod -Method GET -URI $uri -headers $headers
+	  $response
+  }
+  Catch {
+    ResponseException # Call Function ResponseExecption to get error response from the exception
+  }
 }
 Export-ModuleMember -Function Get-VCFCredential
 
