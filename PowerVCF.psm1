7986c20abc83842b1ef12089e82f8af626517e8c
Lakshmanan Shanmugam <lshanmugam@vmware.com>
2023-09-20 11:55:26 +0530

    Fix:  Resolve a review comment. Updates the `Update-VCFIdentityProvider` cmdlet.
    Signed-off-by: Lakshmanan Shanmugam <lshanmugam@vmware.com>

--- PowerVCF.psm1
+++ PowerVCF.psm1
@@ -6182,8 +6182,7 @@ Function Update-VCFIdentityProvider {
                 $id = (Get-VCFIdentityProviderIdByType -Type "Microsoft ADFS")
                 $uri = "https://$sddcManager/v1/identity-providers/$id"
             }
-            $response = Invoke-RestMethod -Method PATCH -Uri $uri -Headers $headers -ContentType application/json -Body $jsonBody
-            $response  # This API does not return a response.
+            Invoke-RestMethod -Method PATCH -Uri $uri -Headers $headers -ContentType application/json -Body $jsonBody # This API does not return a response.
          } else {
             Write-Warning "This API is not supported on this version of VMware Cloud Foundation: $vcfVersion."
         }
